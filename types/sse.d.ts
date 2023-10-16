29c998e1c827921ec4aa489e57a9a5de1eb0bffd
Markus Mohanty <markus.mohanty@dedalus.com>
2023-10-16 14:36:27 +0200

    add types

---
+++ types/sse.d.ts
@@ -0,0 +1,79 @@
+export type SSE = {
+    /**
+     * - headers
+     */
+    headers: string;
+    /**
+     * - payload as a string
+     */
+    payload: string;
+    /**
+     * - HTTP Method
+     */
+    method: string;
+    /**
+     * - flag, if credentials needed
+     */
+    withCredentials: boolean;
+    /**
+     * - debugging flag
+     */
+    debug: boolean;
+};
+export type SSEHeader = {
+    [key: string]: string;
+};
+export type SSEOptions = {
+    /**
+     * - headers
+     */
+    headers: string;
+    /**
+     * - payload as a string
+     */
+    payload: string;
+    /**
+     * - HTTP Method
+     */
+    method: string;
+    /**
+     * - flag, if credentials needed
+     */
+    withCredentials: boolean;
+    /**
+     * - debugging flag
+     */
+    debug: boolean;
+};
+/**
+ * Copyright (C) 2016-2023 Maxime Petazzoni <maxime.petazzoni@bulix.org>.
+ * All rights reserved.
+ */
+/**
+ * @typedef { {[key: string]: string} } SSEHeader
+ */
+/**
+ * @typedef {Object} SSEOptions
+ * @property {string} headers - headers
+ * @property {string} payload - payload as a string
+ * @property {string} method - HTTP Method
+ * @property {boolean} withCredentials - flag, if credentials needed
+ * @property {boolean} debug - debugging flag
+ */
+/**
+ * @typedef {Object} SSE
+ * @property {string} headers - headers
+ * @property {string} payload - payload as a string
+ * @property {string} method - HTTP Method
+ * @property {boolean} withCredentials - flag, if credentials needed
+ * @property {boolean} debug - debugging flag
+ */
+/**
+ * @type SSE
+ * @param {string} url
+ * @param {SSEOptions} options
+ * @return {SSE}
+ * @constructor {SSEConstructor}
+ */
+export var SSE: SSE;
+//# sourceMappingURL=sse.d.ts.map
